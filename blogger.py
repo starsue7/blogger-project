a62bdad923952a7ac989b8fd89a40a0e0222952b
starsue7 <sue_alieva@mail.bg>
2021-01-27 17:32:32 +0200

    adding starting project

---
+++ blogger.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from time import sleep
+
+for i in range(50):
+	my_blog = 'https://livenowisgood.blogspot.com/2019/12/soft-purity-lavera.html'
+	driver = webdriver.Chrome('./chromedriver')
+	driver.get(my_blog)
+	sleep(5)
+	driver.quit()
