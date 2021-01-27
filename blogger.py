09a26edc1e11caaa505d7f949ba3d21cf508f65c
starsue7 <sue_alieva@mail.bg>
2021-01-27 17:59:40 +0200

    import random

--- blogger.py
+++ blogger.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from time import sleep
 from time import time
+from math import random
 
 for i in range(50):
 	my_blog = 'https://livenowisgood.blogspot.com/2019/12/soft-purity-lavera.html'
