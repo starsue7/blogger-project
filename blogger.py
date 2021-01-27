c8f5077f6e9554d7ca7d40afbd611be04f703e13
starsue7 <sue_alieva@mail.bg>
2021-01-27 18:07:23 +0200

    deleting math random

--- blogger.py
+++ blogger.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from time import sleep
 from time import time
-from math import random
 
 for i in range(50):
 	my_blog = 'https://livenowisgood.blogspot.com/2019/12/soft-purity-lavera.html'
